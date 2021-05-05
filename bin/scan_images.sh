eeeddfbe694514ed851e1b35e7d2bb82ec165bf4
OnlyOneV1 <83130730+OnlyOneV1@users.noreply.github.com>
2021-05-05 17:21:45 +0800

    Update scan_images.sh

--- bin/scan_images.sh
+++ bin/scan_images.sh
@@ -1,15 +1,7 @@
 #!/bin/sh
 
-mkdir /tmp/report/
 for image in "$@"
 do
-    cs -r /tmp/report/$(echo $image|md5sum |cut -d' ' -f1).json $image > /dev/null 2> /dev/null
+    cs n $image 
 done
 
-if [ -n "$(ls -A /tmp/report)" ]
-then
-    jq -sM . /tmp/report/* | jq '[.[] | select (.vulnerabilities != []) | {image: .image, vulnerabilities: .vulnerabilities}]'|grep -v '^\[\]$' && exit 1 || exit 0
-else
-    echo "No image scanned."
-    exit 1
-fi
